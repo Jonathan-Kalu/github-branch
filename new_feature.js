f45255524a0bc137bb2b7b14826e7b8e950081c9
Jonathan-Kalu <jonathankalu313@gmail.com>
2024-12-05 23:01:05 +0100

    updated

--- new_feature.js
+++ new_feature.js
@@ -6,3 +6,7 @@ let num2 = 1;
 let total = num2 - num;
 
 alert("Answer is :" + num);
+
+const total1 = total;
+
+console.log(total1);
--- updated.js
+++ updated.js
@@ -1,2 +1,7 @@
 
 console.log("updated feature 2");
+
+let a = 5;
+let b = 5;
+
+console.log(a + b);
